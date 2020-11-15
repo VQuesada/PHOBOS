967f597e999536b1e2b2637e976aa6db748918a4
vquesada <vicquegua@gmail.com>
2020-11-15 17:05:18 +0100

    nuevo carrusel

--- js/store.js
+++ js/store.js
@@ -1,12 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(elementosCarousel, {
-        duration: 150,
-        dist: -10,
-        shift: 5,
-        padding: 5,
-        numVisible: 6,
-        indicators: true,
-        noWrap: false
-    });
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 775,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				draggable: false,
+				itemWidth: 150,
+				duration: 0.25
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				draggable: false,
+				itemWidth: 150,
+				duration: 0.25
+			  }
+			}
+		  ]
+	});
 });
